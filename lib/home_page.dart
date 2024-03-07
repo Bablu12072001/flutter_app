a90d8e4a889c19bac189c73c451165ad2909db3b
Bablu12072001 <bkumar41366@gmail.com>
2024-03-07 13:49:06 +0530

    day2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Bablu";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Study app"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to the $days days flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
